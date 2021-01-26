b01104e5ac4e02c08db76328a13d95ad2cd845ed
huangzhuo <darkmoon4195@qq.com>
2021-01-26 10:13:57 +0800

    t

--- src/pages/system-config/function-module/components/form/rule.ts
+++ src/pages/system-config/function-module/components/form/rule.ts
@@ -1,14 +1,6 @@
 export default {
-    name: [
-        {required: true, message: "模块名称不能为空"}
-    ],
-    category: [
-        {required: true, message: "类别不能为空"}
-    ],
-    authCode: [
-        {required: true, message: "授权码不能为空"}
-    ],
-    required: [
-        {required: true, message: "排序不能为空"}
-    ],
-}
+  name: [{ required: true, message: '模块名称不能为空' }],
+  category: [{ required: true, message: '类别不能为空' }],
+  authCode: [{ required: true, message: '授权码不能为空' }],
+  sort: [{ required: true, message: '排序不能为空' }],
+};
