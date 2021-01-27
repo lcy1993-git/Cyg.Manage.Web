7e1c44b9b5d22d305b5ce1af896baef45d0cc251
lsshavedream <517498696@qq.com>
2021-01-27 18:32:20 +0800

    request请求代码提交

--- src/services/system-config/function-module/function-module.ts
+++ src/services/system-config/function-module/function-module.ts
@@ -43,28 +43,28 @@ export interface TreeDataItem extends FunctionModuleItemParams {
 // 获取模块列表的数据
 export const getFunctionModuleTreeList = (): Promise<TreeDataItem[]> => {
   return cyRequest<TreeDataItem[]>(() =>
-    request(`${baseUrl}/Module/GetTreeList`, { method: 'GET' }),
+    request(`${baseUrl.project}/Module/GetTreeList`, { method: 'GET' }),
   );
 };
 // 新增一条数据
 export const addFunctionModuleItem = (params: FunctionModuleItemParams) => {
-  return cyRequest(() => request(`${baseUrl}/Module/Create`, { method: 'POST', data: params }));
+  return cyRequest(() => request(`${baseUrl.project}/Module/Create`, { method: 'POST', data: params }));
 };
 // 更改状态
 export const updateFunctionItemStatus = (id: string) => {
   return cyRequest(() =>
-    request(`${baseUrl}/Module/ChangeState`, { method: 'GET', params: { id } }),
+    request(`${baseUrl.project}/Module/ChangeState`, { method: 'GET', params: { id } }),
   );
 };
 // 更新数据
 export const updateFunctionModuleItem = (params: TreeDataItem) => {
-  return cyRequest(() => request(`${baseUrl}/Module/Modify`, { method: 'POST', data: params }));
+  return cyRequest(() => request(`${baseUrl.project}/Module/Modify`, { method: 'POST', data: params }));
 };
 
 // 获取一条数据
 export const getFunctionModuleDetail = (id: string) => {
   return cyRequest<ItemDetailData>(() =>
-    request(`${baseUrl}/Module/GetById`, { method: 'GET', params: { id } }),
+    request(`${baseUrl.project}/Module/GetById`, { method: 'GET', params: { id } }),
   );
 };
 
@@ -77,12 +77,12 @@ export interface FunctionModuleTreeData {
 // 下拉选择获取数据
 export const getTreeSelectData = () => {
   return cyRequest<FunctionModuleTreeData[]>(() =>
-    request(`${baseUrl}/Module/GetTree`, { method: 'GET' }),
+    request(`${baseUrl.project}/Module/GetTree`, { method: 'GET' }),
   );
 };
 // 删除
 export const delectFunctionItem = (id: string) => {
   return cyRequest(() =>
-    request(`${baseUrl}/Module/DeleteById`, { method: 'GET', params: { id } }),
+    request(`${baseUrl.project}/Module/DeleteById`, { method: 'GET', params: { id } }),
   );
 };
