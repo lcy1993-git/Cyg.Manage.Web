de38831a0ff248b6216599b65f544c3608a06ba5
lsshavedream <517498696@qq.com>
2021-02-02 15:07:12 +0800

    只读组件提交

--- src/pages/index/test-page/index.tsx
+++ src/pages/index/test-page/index.tsx
@@ -3,11 +3,10 @@ import {Button, Form} from "antd";
 import React from "react";
 import TableImportButton from "@/components/table-import-button";
 import TableExportButton from "@/components/table-export-button";
+import ReadonlyItem from "@/components/readonly-item";
 
 const TestPage = () => {
     const [form] = Form.useForm();
-
-    
 
     return (
         <Form form={form}>
@@ -18,6 +17,20 @@ const TestPage = () => {
             <TableImportButton importUrl="/Dictionary/Import" />
             {/* 导出 */}
             <TableExportButton exportUrl="/Dictionary/Export" />
+            <ReadonlyItem label="反馈用户2">
+                1355027394511482881
+            </ReadonlyItem>
+            <ReadonlyItem label="反馈用户">
+                1355027394511482881
+            </ReadonlyItem>
+            <ReadonlyItem label="反馈用户">
+                1355027394511482881
+            </ReadonlyItem>
+            <ReadonlyItem label="反馈用户">
+                1355027394511482881
+            </ReadonlyItem>
+            
+
         </Form>
     )
 }
