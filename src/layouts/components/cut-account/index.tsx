41fa54468348e9ac97c3780c059ace176aac4d7c
huangzhuo <darkmoon4195@qq.com>
2021-04-01 15:54:42 +0800

    修改安排bug

--- src/layouts/components/cut-account/index.tsx
+++ src/layouts/components/cut-account/index.tsx
@@ -1,69 +1,93 @@
-import CyFormItem from "@/components/cy-form-item";
-import { userLoginRequest } from "@/services/login";
-import { flatten } from "@/utils/utils";
-import { useControllableValue } from "ahooks";
-import { Form, Input, message, Modal } from "antd";
-import React, { Dispatch } from "react";
-import { SetStateAction } from "react";
+import CyFormItem from '@/components/cy-form-item';
+import { userLoginRequest } from '@/services/login';
+import { flatten } from '@/utils/utils';
+import { useControllableValue } from 'ahooks';
+import { Form, Input, message, Modal } from 'antd';
+import React, { Dispatch } from 'react';
+import { SetStateAction } from 'react';
 
 interface EditPasswordProps {
-    visible: boolean
-    onChange: Dispatch<SetStateAction<boolean>>
+  visible: boolean;
+  onChange: Dispatch<SetStateAction<boolean>>;
 }
 
 const CutAccount = (props: EditPasswordProps) => {
-    const [state, setState] = useControllableValue(props, { valuePropName: "visible" });
+  const [state, setState] = useControllableValue(props, { valuePropName: 'visible' });
 
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
-    const sureCutAccount = () => {
-        form.validateFields().then(async (value) => {
-            const { userName, pwd } = value;
-            // TODO 快捷切换
-            const resData = await userLoginRequest({ userName, pwd });
+  const sureCutAccount = () => {
+    form.validateFields().then(async (value) => {
+      const { userName, pwd } = value;
+      // TODO 快捷切换
+      const resData = await userLoginRequest({ userName, pwd });
 
-            const { accessToken, modules, user } = resData;
+      const { accessToken, modules, user } = resData;
 
-            const buttonModules = flatten(modules);
-            const buttonArray = buttonModules.filter((item: any) => item.category === 3).map((item: any) => item.authCode);
+      const buttonModules = flatten(modules);
+      const buttonArray = buttonModules
+        .filter((item: any) => item.category === 3)
+        .map((item: any) => item.authCode);
 
-            localStorage.setItem('Authorization', accessToken);
-            localStorage.setItem('functionModules', JSON.stringify(modules));
-            localStorage.setItem('userInfo', JSON.stringify(user));
-            localStorage.setItem('buttonJurisdictionArray', JSON.stringify(buttonArray));
+      localStorage.setItem('Authorization', accessToken);
+      localStorage.setItem('functionModules', JSON.stringify(modules));
+      localStorage.setItem('userInfo', JSON.stringify(user));
+      localStorage.setItem('buttonJurisdictionArray', JSON.stringify(buttonArray));
 
-            setState(false)
-            message.success("账户切换成功")
-            location.reload();
-        })
+      setState(false);
+      message.success('账户切换成功');
+      location.reload();
+    });
+  };
+
+  const onKeyDownLogin = (e: any) => {
+    if (e.keyCode == 13) {
+      sureCutAccount();
     }
+  };
 
-    const onKeyDownLogin = (e: any) => {
-        if (e.keyCode == 13) {
-            sureCutAccount();
-        }
-      };
+  return (
+    <Modal
+      title="快捷登录"
+      visible={state as boolean}
+      destroyOnClose
+      okText="确定"
+      cancelText="取消"
+      onCancel={() => setState(false)}
+      onOk={() => sureCutAccount()}
+    >
+      <Form form={form} preserve={false} onKeyDown={(e) => onKeyDownLogin(e)}>
+        <CyFormItem
+          name="userName"
+          label="用户名"
+          required
+          labelWidth={100}
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名',
+            },
+          ]}
+        >
+          <Input placeholder="请输入" />
+        </CyFormItem>
+        <CyFormItem
+          name="pwd"
+          label="密码"
+          required
+          labelWidth={100}
+          rules={[
+            {
+              required: true,
+              message: '密码',
+            },
+          ]}
+        >
+          <Input type="password" placeholder="请输入" />
+        </CyFormItem>
+      </Form>
+    </Modal>
+  );
+};
 
-    return (
-        <Modal title="快捷登录" visible={state as boolean} destroyOnClose okText="确定" cancelText="取消" onCancel={() => setState(false)} onOk={() => sureCutAccount()}>
-            <Form form={form} preserve={false} onKeyDown={(e)=>onKeyDownLogin(e)}>
-                <CyFormItem name="userName" label="用户名" required labelWidth={100} rules={[
-                    {
-                        required: true, message: "请输入用户名"
-                    }
-                ]}>
-                    <Input placeholder="请输入" />
-                </CyFormItem>
-                <CyFormItem name="pwd" label="密码" required labelWidth={100} rules={[
-                    {
-                        required: true, message: "密码"
-                    }
-                ]}>
-                    <Input type="password" placeholder="请输入" />
-                </CyFormItem>
-            </Form>
-        </Modal>
-    )
-}
-
-export default CutAccount
+export default CutAccount;
