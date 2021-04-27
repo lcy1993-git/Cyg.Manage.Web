c33d6f594e66d88d51d789e066429ae1d1d48bc7
psj <271533323@qq.com>
2021-04-27 14:35:04 +0800

    feat: 分离group图层控制的全局状态

--- src/pages/visualization-results/components/control-layers/index.tsx
+++ src/pages/visualization-results/components/control-layers/index.tsx
@@ -1,13 +1,12 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Checkbox, Divider } from 'antd';
 import Icon from '@ant-design/icons';
 
-import { ControlLayearsData } from '../../utils';
 import styles from './index.less';
 
 const LayereIcon = () => (
   <svg
-    t="1619065560359"
+    // t="1619065560359"
     className="icon"
     viewBox="0 0 1024 1024"
     version="1.1"
@@ -23,20 +22,28 @@ const LayereIcon = () => (
     ></path>
   </svg>
 );
-type ListProps = {
-  onLayersStateChange: (arg0: number) => void;
-} & ControlLayearsData;
+interface ListProps {
+  name: string;
+  state: boolean;
+  onChange: () => void;
+};
 
 interface Props {
-  controlLayearsData: ControlLayearsData[];
-  onLayersStateChange: (arg0: number) => void;
+  surveyLayerVisible: boolean;
+  planLayerVisible: boolean;
+  designLayerVisible: boolean;
+  dismantleLayerVisible: boolean;
+  setSurveyLayerVisible: (arg0: boolean) => void;
+  setPlanLayerVisible: (arg0: boolean) => void;
+  setDesignLayerVisible: (arg0: boolean) => void;
+  setDismantleLayerVisible: (arg0: boolean) => void;
 }
 
 const ListItem = (props: ListProps) => {
-  const { name, state, index, onLayersStateChange } = props;
+  const { name, state, onChange } = props;
   return (
-    <div className={styles.listItem}>
-      <Checkbox defaultChecked={state} style={{}} onChange={() => onLayersStateChange(index)}>
+    <div className={styles.listItem} key={"listItem" + name}>
+      <Checkbox defaultChecked={state} onChange={onChange}>
         {name}
       </Checkbox>
     </div>
@@ -45,24 +52,9 @@ const ListItem = (props: ListProps) => {
 
 const ControlLayers = (props: Props) => {
   const [visiabel, setVisiabel] = useState<boolean>(false);
-  const { controlLayearsData, onLayersStateChange } = props;
-  const ListItemNode = useMemo(() => {
-    return controlLayearsData.map((item: ControlLayearsData, index: number) => {
-      if (index === 0)
-        return (
-          <div className={styles.listItem} key={item.index}>
-            <ListItem onLayersStateChange={onLayersStateChange} {...item} />
-          </div>
-        );
-      return (
-        <div className={styles.listItem} key={item.index}>
-          <Divider style={{ margin: 2 }} />
-          <ListItem onLayersStateChange={onLayersStateChange} {...item} />
-        </div>
-      );
-    });
-  }, [JSON.stringify(controlLayearsData)]);
+  const { surveyLayerVisible, planLayerVisible, designLayerVisible, dismantleLayerVisible, setSurveyLayerVisible, setPlanLayerVisible, setDesignLayerVisible, setDismantleLayerVisible} = props;
 
+  console.log(surveyLayerVisible, planLayerVisible, designLayerVisible, dismantleLayerVisible)
   return (
     <div className={styles.container}>
       <div
@@ -71,7 +63,25 @@ const ControlLayers = (props: Props) => {
         onMouseLeave={() => setVisiabel(false)}
       >
         <Icon style={{backgroundColor: "rgba(255,255,255, 0.6)", padding: "3px"}} component={LayereIcon} />
-        {visiabel && <div className={styles.list}>{ListItemNode}</div>}
+        {visiabel &&
+          <div className={styles.list}>
+            <div className={styles.listItem} key="surveyLayerVisible">
+              <ListItem  name="勘察图层" state={surveyLayerVisible} onChange={() => setSurveyLayerVisible(!surveyLayerVisible)} />
+            </div>
+            <div className={styles.listItem} key="planLayerVisible">
+              <Divider style={{ margin: 2 }} />
+              <ListItem  name="方案图层" state={planLayerVisible} onChange={() => setPlanLayerVisible(!planLayerVisible)} />
+            </div>
+            <div className={styles.listItem} key="designLayerVisible">
+              <Divider style={{ margin: 2 }} />
+              <ListItem  name="设计图层" state={designLayerVisible} onChange={() => setDesignLayerVisible(!designLayerVisible)}/>
+            </div>
+            <div className={styles.listItem} key="dismantleLayerVisible">
+              <Divider style={{ margin: 2 }} />
+              <ListItem  name="拆除图层" state={dismantleLayerVisible} onChange={() => setDismantleLayerVisible(!dismantleLayerVisible)}/>
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
