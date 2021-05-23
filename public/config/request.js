ff2b155bd82ed474277d0ed212bbe6f52986c777
yangl <517498696@qq.com>
2021-05-23 23:49:26 +0800

    feat: 1.0.55版本打包提交

--- public/config/request.js
+++ public/config/request.js
@@ -74,5 +74,5 @@ export const areaStatisticsUrl = ipArray.includes(thisHostName) ? `${baseUrl}:80
 
 export const serverCodeArray = {hostName:`${thisHostName}`}
 
-export const version = "1.0.54";
+export const version = "1.0.55";
 
