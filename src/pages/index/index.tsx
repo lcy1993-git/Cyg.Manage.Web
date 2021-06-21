23ff6524eb5516230bc781df781d5c4ada24aaec
psj <271533323@qq.com>
2021-06-21 09:56:22 +0800

    fix: 修改首页窗口改动布局不生效bug

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { memo, useState, useMemo } from 'react';
 import styles from './index.less';
 import { WidthProvider, Responsive } from 'react-grid-layout';
 import bgSrc from '@/assets/image/index/bg.png';
-import { useRequest } from 'ahooks';
+import { useRequest, useSize } from 'ahooks';
 import { getChartConfig } from '@/services/operation-config/cockpit';
 import { useRef } from 'react';
 
@@ -21,10 +21,9 @@ import ProjectProgress from '@/pages/index/components/index-project-progress-com
 
 import { IndexContext } from './context';
 import { Spin } from 'antd';
-import { divide, multiply, subtract } from 'lodash';
-import uuid from 'node-uuid';
 import PageCommonWrap from '@/components/page-common-wrap';
 import ProjectRefreshListWrapper from './components/refresh-list-wrapper/idnex';
+import initConfigArray from './utils/initConfigArray';
 
 const getComponentByType = (type: string, componentProps: any) => {
   switch (type) {
@@ -63,73 +62,76 @@ const Index: React.FC = () => {
     areaLevel: '1',
   });
 
-  const [configArray, setConfigArray] = useState<any[]>([]);
-
   const { data, loading } = useRequest(() => getChartConfig(), {
-    onSuccess: () => {
-      const windowHeight = window.innerHeight - 115 > 828 ? window.innerHeight - 115 : 828;
-      if (data) {
-        const hasSaveConfig = JSON.parse(data);
-        if (hasSaveConfig.config && hasSaveConfig.config.length > 0) {
-          const windowPercent = divide(windowHeight, hasSaveConfig.configWindowHeight);
-          const thisConfigArray = hasSaveConfig.config.map((item: any) => {
-            const actualHeight = windowPercent
-              ? Math.floor(multiply(item.h, windowPercent) * 100) / 100
-              : item.h;
-            const actualY = windowPercent ? multiply(item.y, windowPercent) : item.y;
-            return {
-              ...item,
-              y: actualY,
-              h: actualHeight,
-            };
-          });
-          setConfigArray(thisConfigArray);
-        }
-      } else {
-        const thisBoxHeight = windowHeight - 75;
-        const totalHeight = divide(thisBoxHeight, 18);
-        setConfigArray([
-          { name: 'toDo', x: 0, y: 0, w: 3, h: 11, key: uuid.v1() },
-          {
-            name: 'mapComponent',
-            x: 3,
-            y: 0,
-            w: 6,
-            h: subtract(totalHeight, divide(totalHeight - 11, 2)),
-            key: uuid.v1(),
-          },
-          { name: 'projectType', x: 9, y: 0, w: 3, h: 11, key: uuid.v1() },
-          {
-            name: 'projectRefreshData',
-            x: 0,
-            y: 11,
-            w: 3,
-            h: divide(totalHeight - 11, 2),
-            key: uuid.v1(),
-          },
-          { name: 'personLoad', x: 9, y: 11, w: 3, h: divide(totalHeight - 11, 2), key: uuid.v1() },
-          {
-            name: 'deliveryManage',
-            x: 0,
-            y: divide(totalHeight - 11, 2) + 11,
-            w: 6,
-            h: divide(totalHeight - 11, 2),
-            key: uuid.v1(),
-          },
-          {
-            name: 'projectProgress',
-            x: 6,
-            y: divide(totalHeight - 11, 2) + 11,
-            w: 6,
-            h: divide(totalHeight - 11, 2),
-            key: uuid.v1(),
-          },
-        ]);
-      }
-    },
+    // onSuccess: () => {
+    //   const windowHeight = window.innerHeight - 115 > 828 ? window.innerHeight - 115 : 828;
+    //   if (data) {
+    //     const hasSaveConfig = JSON.parse(data);
+    //     if (hasSaveConfig.config && hasSaveConfig.config.length > 0) {
+    //       const windowPercent = divide(windowHeight, hasSaveConfig.configWindowHeight);
+    //       const thisConfigArray = hasSaveConfig.config.map((item: any) => {
+    //         const actualHeight = windowPercent
+    //           ? Math.floor(multiply(item.h, windowPercent) * 100) / 100
+    //           : item.h;
+    //         const actualY = windowPercent ? multiply(item.y, windowPercent) : item.y;
+    //         return {
+    //           ...item,
+    //           y: actualY,
+    //           h: actualHeight,
+    //         };
+    //       });
+    //       setConfigArray(thisConfigArray);
+    //     }
+    //   } else {
+    //     const thisBoxHeight = windowHeight - 75;
+    //     const totalHeight = divide(thisBoxHeight, 18);
+    //     setConfigArray([
+    //       { name: 'toDo', x: 0, y: 0, w: 3, h: 11, key: uuid.v1() },
+    //       {
+    //         name: 'mapComponent',
+    //         x: 3,
+    //         y: 0,
+    //         w: 6,
+    //         h: subtract(totalHeight, divide(totalHeight - 11, 2)),
+    //         key: uuid.v1(),
+    //       },
+    //       { name: 'projectType', x: 9, y: 0, w: 3, h: 11, key: uuid.v1() },
+    //       {
+    //         name: 'projectRefreshData',
+    //         x: 0,
+    //         y: 11,
+    //         w: 3,
+    //         h: divide(totalHeight - 11, 2),
+    //         key: uuid.v1(),
+    //       },
+    //       { name: 'personLoad', x: 9, y: 11, w: 3, h: divide(totalHeight - 11, 2), key: uuid.v1() },
+    //       {
+    //         name: 'deliveryManage',
+    //         x: 0,
+    //         y: divide(totalHeight - 11, 2) + 11,
+    //         w: 6,
+    //         h: divide(totalHeight - 11, 2),
+    //         key: uuid.v1(),
+    //       },
+    //       {
+    //         name: 'projectProgress',
+    //         x: 6,
+    //         y: divide(totalHeight - 11, 2) + 11,
+    //         w: 6,
+    //         h: divide(totalHeight - 11, 2),
+    //         key: uuid.v1(),
+    //       },
+    //     ]);
+    //   }
+    // },
   });
 
   const divRef = useRef<HTMLDivElement>(null);
+  const { width, height } = useSize(divRef);
+
+  const configArray = useMemo(() => {
+    return initConfigArray(data);
+  }, [width, height, JSON.stringify(data)])
 
   const configComponentElement = configArray?.map((item: any) => {
     return (
@@ -148,8 +150,8 @@ const Index: React.FC = () => {
         }}
       >
         <div className={styles.indexPage} style={{ backgroundImage: `url(${bgSrc})` }} ref={divRef}>
-          {!loading && (
-            <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+          { !loading &&
+            <div key={new Date().getTime()} style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
               <ResponsiveReactGridLayout
                 style={{ position: 'relative' }}
                 breakpoints={{ lg: 120 }}
@@ -159,7 +161,7 @@ const Index: React.FC = () => {
                 {configComponentElement}
               </ResponsiveReactGridLayout>
             </div>
-          )}
+          }
         </div>
         {loading && (
           <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
