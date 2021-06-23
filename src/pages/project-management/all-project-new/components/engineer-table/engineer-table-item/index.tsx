7eca03ee04462796c30a0cae559c75a09d43eec7
yangl <517498696@qq.com>
2021-06-23 16:46:51 +0800

    feat: 组件代码提交

--- src/pages/project-management/all-project-new/components/engineer-table/engineer-table-item/index.tsx
+++ src/pages/project-management/all-project-new/components/engineer-table/engineer-table-item/index.tsx
@@ -1,15 +1,251 @@
-import React from 'react';
-import styles from './index.less';
+import React, { useMemo } from 'react';
+import { CheckboxValueType } from 'antd/lib/checkbox/Group';
+import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Tooltip } from 'antd';
+import { useBoolean } from 'ahooks';
 
-const EngineerTableItem: React.FC = () => {
+import styles from './index.less';
+import moment from 'moment';
+import { useGetButtonJurisdictionArray } from '@/utils/hooks';
+import uuid from 'node-uuid';
+import EmptyTip from '@/components/empty-tip';
+
+export interface AddProjectValue {
+  engineerId: string;
+  areaId: string;
+  company: string;
+  companyName: string;
+}
+
+interface EngineerTableItemProps {
+  projectInfo: any;
+  columns: any[];
+  onChange?: (checkedValue: TableItemCheckedInfo) => void;
+  getClickProjectId: (clickProjectId: string) => void;
+  addProject?: (needValue: AddProjectValue) => void;
+  editEngineer?: (needValue: AddProjectValue) => void;
+}
+interface TableCheckedItemProjectInfo {
+  id: string;
+  isAllChecked: boolean;
+  status?: any;
+  name?: any;
+}
+
+export interface TableItemCheckedInfo {
+  projectInfo: TableCheckedItemProjectInfo;
+  checkedArray: string[];
+}
+
+const EngineerTableItem: React.FC<EngineerTableItemProps> = (props) => {
+  const [isFold, { toggle: foldEvent }] = useBoolean(false);
+
+  const [checkedList, setCheckedList] = React.useState<CheckboxValueType[]>([]);
+  const [indeterminate, setIndeterminate] = React.useState(false);
+  const [checkAll, setCheckAll] = React.useState(false);
+
+  const buttonJurisdictionArray = useGetButtonJurisdictionArray();
+
+  const {
+    projectInfo = {},
+    columns = [],
+    onChange,
+    getClickProjectId,
+    addProject,
+    editEngineer,
+  } = props;
+
+  const valueList = useMemo(() => {
+    if (projectInfo.projects) {
+      return projectInfo.projects.map((item: any) => item.id);
+    }
+    return [];
+  }, [JSON.stringify(projectInfo.projects)]);
+
+  const checkboxChange = (list: CheckboxValueType[]) => {
+    setCheckedList(list);
+    setIndeterminate(!!list.length && list.length < valueList.length);
+    setCheckAll(list.length === valueList.length);
+
+    onChange?.({
+      projectInfo: {
+        id: projectInfo.id,
+        isAllChecked: list.length === valueList.length,
+        status: projectInfo.projects
+          .map((item: any) => {
+            if (list.includes(item.id)) {
+              return item.stateInfo;
+            }
+          })
+          .filter(Boolean),
+        name: projectInfo.projects
+          .map((item: any) => {
+            if (list.includes(item.id)) {
+              return item.name;
+            }
+          })
+          .filter(Boolean),
+      },
+      checkedArray: list as string[],
+    });
+  };
+
+  const checkAllEvent = (e: any) => {
+    setCheckedList(e.target.checked ? valueList : []);
+    setIndeterminate(false);
+    setCheckAll(e.target.checked);
+
+    onChange?.({
+      projectInfo: {
+        id: projectInfo.id,
+        isAllChecked: e.target.checked,
+        status: projectInfo.projects
+          .map((item: any) => {
+            if (valueList.includes(item.id)) {
+              return item.stateInfo;
+            }
+          })
+          .filter(Boolean),
+        name: projectInfo.projects
+          .map((item: any) => {
+            if (valueList.includes(item.id)) {
+              return item.name;
+            }
+          })
+          .filter(Boolean),
+      },
+      checkedArray: e.target.checked ? valueList : [],
+    });
+  };
+
+  const projectNameClickEvent = (projectId: string) => {
+    getClickProjectId?.(projectId);
+  };
+
+  const addProjectEvent = () => {
+    addProject?.({
+      engineerId: projectInfo.id,
+      areaId: projectInfo.province,
+      company: projectInfo.company,
+      companyName: projectInfo.company,
+    });
+  };
+
+  const editEngineerEvent = () => {
+    editEngineer?.({
+      engineerId: projectInfo.id,
+      areaId: projectInfo.province,
+      company: projectInfo.company,
+      companyName: projectInfo.company,
+    });
+  };
+
+  const theadElement = columns.map((item) => {
+    return (
+      <div
+        className={styles.engineerTableTh}
+        key={uuid.v1()}
+        style={item.width ? { width: `${item.width}` } : { flex: '1' }}
+      >
+        {item.title}
+      </div>
+    );
+  });
+
+  const tbodyElement = (projectInfo.projects ?? []).map((item: any) => {
+    return (
+      <div key={uuid.v1()} className={styles.engineerTableTr}>
+        <div className={styles.engineerTableTd} style={{ width: '44px' }}>
+          <Checkbox style={{ marginLeft: '4px' }} value={item.id} />
+        </div>
+        {columns.map((ite) => {
+          return (
+            <div
+              className={styles.engineerTableTd}
+              key={uuid.v1()}
+              style={ite.width ? { width: `${ite.width}` } : { flex: '1' }}
+            >
+              {ite.render ? ite.render(item, projectInfo) : item[ite.dataIndex]}
+            </div>
+          );
+        })}
+      </div>
+    );
+  });
+
   return (
     <div className={styles.engineerTableItem}>
-        <div className={styles.engineerTableItemHeader}>
-            
+      <div className={styles.engineerTableItemHeader}>
+        <div className={styles.foldButton} onClick={() => foldEvent()}>
+          <span>{isFold ? <CaretUpOutlined /> : <CaretDownOutlined />}</span>
         </div>
-        <div className={styles.engineerTableBody}>
-
+        <div className={styles.projectName}>
+          <Checkbox
+            onChange={checkAllEvent}
+            style={{ marginRight: '7px' }}
+            indeterminate={indeterminate}
+            checked={checkAll}
+          />
+          <Tooltip title={projectInfo.name}>
+            <u className="canClick" onClick={() => projectNameClickEvent(projectInfo.id)}>
+              {projectInfo.name}
+            </u>
+          </Tooltip>
         </div>
+        <div className={styles.projectAccount}>
+          <span className={styles.label}>共有项目:</span>
+          <span>{projectInfo.projects ? projectInfo.projects?.length : 0}个</span>
+        </div>
+        <div className={styles.projectTime}>
+          <span className={styles.label}>工程日期:</span>
+          <span>
+            {projectInfo.startTime ? moment(projectInfo.startTime).format('YYYY/MM/DD') : ''}
+          </span>
+          <span>
+            -{projectInfo.startTime ? moment(projectInfo.endTime).format('YYYY/MM/DD') : ''}
+          </span>
+        </div>
+        <div className={styles.createTime}>
+          <span className={styles.label}>编制日期:</span>
+          <span>
+            {projectInfo.compileTime ? moment(projectInfo.compileTime).format('YYYY/MM/DD') : ''}
+          </span>
+        </div>
+        <div className={styles.projectButtons}>
+          {projectInfo?.operationAuthority?.canAddProject &&
+            buttonJurisdictionArray?.includes('all-project-add-project') && (
+              <Button className="mr10" ghost type="primary" onClick={() => addProjectEvent()}>
+                新增项目
+              </Button>
+            )}
+          {projectInfo?.operationAuthority?.canEdit &&
+            buttonJurisdictionArray?.includes('all-project-edit-engineer') && (
+              <Button onClick={() => editEngineerEvent()}>编辑</Button>
+            )}
+        </div>
+      </div>
+      <div className={styles.engineerTableBody}>
+        {!isFold && projectInfo.projects && projectInfo.projects.length > 0 && (
+          <Checkbox.Group value={checkedList} onChange={checkboxChange}>
+            <div className={styles.engineerTable}>
+              <div className={styles.engineerTableContent}>
+                <div className={styles.engineerTableHeader}>
+                  <div className={styles.engineerTableTh} style={{ width: '44px' }}></div>
+                  {theadElement}
+                </div>
+                <div className={styles.engineerTableBody}>{tbodyElement}</div>
+              </div>
+            </div>
+          </Checkbox.Group>
+        )}
+        {!isFold &&
+          (!projectInfo.projects ||
+            (projectInfo.projects && projectInfo.projects.length === 0)) && (
+            <div className={styles.noEngineerData}>
+              <EmptyTip className="pt20 pb20" description="该工程下没有项目" />
+            </div>
+          )}
+      </div>
     </div>
   );
 };
--- src/pages/project-management/all-project-new/components/engineer-table/index.tsx
+++ src/pages/project-management/all-project-new/components/engineer-table/index.tsx
@@ -1,33 +1,146 @@
+import {
+  AllProjectStatisticsParams,
+  getProjectTableList,
+} from '@/services/project-management/all-project';
+import { useGetButtonJurisdictionArray } from '@/utils/hooks';
+import { useRequest } from 'ahooks';
+import { Menu } from 'antd';
 import { Pagination } from 'antd';
 import React from 'react';
+import { useMemo } from 'react';
 import { useState } from 'react';
+import EngineerTableItem from './engineer-table-item';
 import styles from './index.less';
 
-const EngineerTable: React.FC = () => {
+interface ExtractParams extends AllProjectStatisticsParams {
+  statisticalCategory?: string;
+}
+
+interface EngineerTableProps {
+  extractParams: ExtractParams;
+  onSelect?: (checkedValue: TableItemCheckedInfo[]) => void;
+  afterSearch?: () => void;
+  delayRefresh?: () => void;
+}
+
+const EngineerTable: React.FC<EngineerTableProps> = (props) => {
+  const { extractParams, onSelect, afterSearch, delayRefresh, getStatisticsData } = props;
   const [pageIndex, setPageIndex] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(10);
 
-  const pageSizeChange = () => {
+  const { data: tableData, loading, run } = useRequest(getProjectTableList, { manual: true });
 
-  }
+  const buttonJurisdictionArray = useGetButtonJurisdictionArray();
 
-  const currentPageChange = () => {
-      
-  }
+  const tableResultData = useMemo(() => {
+    if (tableData) {
+      const { pagedData, statistics } = tableData;
+      const { items, pageIndex, pageSize, total } = pagedData;
+      getStatisticsData?.(statistics);
+      return {
+        items: items ?? [],
+        pageIndex,
+        pageSize,
+        total,
+        dataStartIndex: Math.floor((pageIndex - 1) * pageSize + 1),
+        dataEndIndex: Math.floor((pageIndex - 1) * pageSize + (items ?? []).length),
+        projectLen:
+          items
+            ?.filter((item: any) => item.projects && item.projects.length > 0)
+            .map((item: any) => item.projects)
+            .flat().length ?? 0,
+      };
+    }
+    return {
+      items: [],
+      pageIndex: 1,
+      pageSize: 20,
+      total: 0,
+      dataStartIndex: 0,
+      dataEndIndex: 0,
+      projectLen: 0,
+    };
+  }, [JSON.stringify(tableData)]);
 
-  const tableResultData = {
-    items: [],
-    pageIndex: 1,
-    pageSize: 20,
-    total: 0,
-    dataStartIndex: 0,
-    dataEndIndex: 0,
-    projectLen: 0,
+  const projectItemMenu = (
+    jurisdictionInfo: JurisdictionInfo,
+    tableItemData: any,
+    engineerInfo: any,
+    status: any,
+  ) => {
+    return (
+      <Menu>
+        {jurisdictionInfo.canEdit && buttonJurisdictionArray?.includes('all-project-edit-project') && (
+          <Menu.Item
+            onClick={() => {
+              editProjectEvent({
+                projectId: tableItemData.id,
+                areaId: engineerInfo.province,
+                company: engineerInfo.company,
+                companyName: engineerInfo.company,
+                status: tableItemData.stateInfo.status,
+              });
+            }}
+          >
+            编辑
+          </Menu.Item>
+        )}
+        {jurisdictionInfo.canCopy && buttonJurisdictionArray?.includes('all-project-copy-project') && (
+          <Menu.Item
+            onClick={() =>
+              copyProjectEvent({
+                projectId: tableItemData.id,
+                areaId: engineerInfo.province,
+                company: engineerInfo.company,
+                engineerId: engineerInfo.id,
+                companyName: engineerInfo.company,
+              })
+            }
+          >
+            复制项目
+          </Menu.Item>
+        )}
+        {buttonJurisdictionArray?.includes('all-project-check-result') && (
+          <Menu.Item
+            onClick={() =>
+              checkResult({
+                projectId: tableItemData.id,
+                projectName: tableItemData.name,
+                projectStatus: tableItemData.stateInfo.statusText,
+                projectStage: tableItemData.stageText,
+              })
+            }
+          >
+            查看成果
+          </Menu.Item>
+        )}
+      </Menu>
+    );
   };
+
+  const pageSizeChange = () => {};
+
+  const currentPageChange = () => {};
+
+  const engineerTableElement = tableResultData?.items.map((item: any, index: number) => {
+    return (
+      <EngineerTableItem
+        // editEngineer={editEngineerEvent}
+        // addProject={addProjectEvent}
+        // getClickProjectId={projectNameClickEvent}
+        // onChange={tableItemSelectEvent}
+        // columns={projectTableColumns}
+        key={item.id}
+        projectInfo={item}
+      />
+    );
+  });
 
   return (
     <div className={styles.engineerTable}>
-      <div className={styles.engineerTableContent}></div>
+      <div className={styles.engineerTableContent}>
+        {engineerTableElement}
+      </div>
       <div className={styles.engineerTablePaging}>
         <div className={styles.engineerTablePagingLeft}>
           <span>显示第</span>
