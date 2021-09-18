5e7ac5c31a5e33e5b5250f0e8628c6a58a99ed31
huangzhuo <darkmoon4195@qq.com>
2021-09-18 15:15:41 +0800

    fix(还原代码): 取消屏蔽待处理事务接口调用

--- src/pages/index/components/to-do/index.tsx
+++ src/pages/index/components/to-do/index.tsx
@@ -1,43 +1,47 @@
-import { getToDoStatistics, AreaInfo } from '@/services/index';
-import { useRequest } from 'ahooks';
-import React from 'react';
-import ChartBox from '../chart-box';
-import ToDoItem from '../to-do-item';
+import { getToDoStatistics, AreaInfo } from "@/services/index"
+import { useRequest } from "ahooks"
+import React from "react"
+import ChartBox from "../chart-box"
+import ToDoItem from "../to-do-item"
 
-interface ToDoProps {
-  componentProps?: string[];
-  currentAreaInfo: AreaInfo;
+interface ToDoProps { 
+    componentProps?: string[];
+    currentAreaInfo: AreaInfo;
 }
 
 const ToDo: React.FC<ToDoProps> = (props) => {
-  const { componentProps = ['wait', 'arrange', 'other'], currentAreaInfo } = props;
 
-  // const { data: toDoStatisticsInfo } = useRequest(() => getToDoStatistics({areaCode: currentAreaInfo.areaId,areaType: currentAreaInfo.areaLevel}), {
-  //     refreshDeps: [currentAreaInfo],
-  //     pollingWhenHidden: false
-  // })
+    const { componentProps = ["wait", "arrange", "other"],currentAreaInfo} = props;
 
-  return (
-    <ChartBox title="待处理事务">
-      <div className="flex">
-        {componentProps.includes('wait') && (
-          <div className="flex1">
-            {/* <ToDoItem icon="wait-review" number={toDoStatisticsInfo?.awaitKnot ?? 0} status={"待结项"} /> */}
-          </div>
-        )}
-        {componentProps.includes('arrange') && (
-          <div className="flex1">
-            {/* <ToDoItem icon="wait-plan" number={toDoStatisticsInfo?.awaitAllot ?? 0} status={"待安排"} /> */}
-          </div>
-        )}
-        {componentProps.includes('other') && (
-          <div className="flex1">
-            <ToDoItem icon="other" number={0} status={'其他消息'} />
-          </div>
-        )}
-      </div>
-    </ChartBox>
-  );
-};
+    const { data: toDoStatisticsInfo } = useRequest(() => getToDoStatistics({areaCode: currentAreaInfo.areaId,areaType: currentAreaInfo.areaLevel}), {
+        refreshDeps: [currentAreaInfo],
+        pollingWhenHidden: false
+    })
 
-export default ToDo;
+    return (
+        <ChartBox title="待处理事务">
+            <div className="flex">
+                {
+                    componentProps.includes("wait") &&
+                    <div className="flex1">
+                        <ToDoItem icon="wait-review" number={toDoStatisticsInfo?.awaitKnot ?? 0} status={"待结项"} />
+                    </div>
+                }
+                {
+                    componentProps.includes("arrange") &&
+                    <div className="flex1">
+                        <ToDoItem icon="wait-plan" number={toDoStatisticsInfo?.awaitAllot ?? 0} status={"待安排"} />
+                    </div>
+                }
+                {
+                    componentProps.includes("other") &&
+                    <div className="flex1">
+                        <ToDoItem icon="other" number={0} status={"其他消息"} />
+                    </div>
+                }
+            </div>
+        </ChartBox>
+    )
+}
+
+export default ToDo
