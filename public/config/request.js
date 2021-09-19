bab9f02de0e9e3a83cc68be14add3d3a11ca254e
yangl <517498696@qq.com>
2021-09-19 14:07:09 +0800

    feat(1.0.119版本): 1.0.119 甘肃专版提交

--- public/config/request.js
+++ public/config/request.js
@@ -77,5 +77,5 @@ export const areaStatisticsUrl = ipArray.includes(thisHostName) ? `${baseUrl}:80
 
 export const serverCodeArray = {hostName:`${thisHostName}`}
 
-export const version = "1.0.116";
+export const version = "1.0.119";
 
