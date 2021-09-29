667ef8662e7683bec554bcee167545b17b481d99
yangl <517498696@qq.com>
2021-09-29 18:20:29 +0800

    feat(所有项目提交): 所有项目跳转BUG提交

--- src/layouts/context.tsx
+++ src/layouts/context.tsx
@@ -1,3 +1,4 @@
+import { AreaInfo } from '@/services/index';
 import { createContext, useContext } from 'react';
 
 interface LayoutContextValue {
@@ -7,11 +8,13 @@ interface LayoutContextValue {
   allProjectSearchProjectId?: string;
   allProjectSearchPerson?: string;
   allProjectSearchType?: string;
+  allProjectAreaInfo?: AreaInfo;
   setResourceManageFlag?: (value: boolean) => void;
   setWorkHandoverFlag?: (value: boolean) => void;
   setAllProjectSearchProjectId?: (value: string) => void;
   setAllProjectSearchPerson?: (value: string) => void;
-  setAllProjectSearchType?:  (value: string) => void;
+  setAllProjectSearchType?: (value: string) => void;
+  setAllProjectAreaInfo?: (value?: AreaInfo) => void;
   removeTab?: (value: string) => void;
 }
 
