6d18e5e5970839f558774ce7e6863e8eadf14469
huangzhuo <darkmoon4195@qq.com>
2021-10-09 14:24:09 +0800

    feat(待办模块): route添加

---
+++ src/pages/project-management/project-entrust/index.tsx
@@ -0,0 +1,13 @@
+import CommonTitle from '@/components/common-title';
+import PageCommonWrap from '@/components/page-common-wrap';
+import React from 'react';
+
+const ProjectEntrust: React.FC = () => {
+  return (
+    <PageCommonWrap>
+      <CommonTitle></CommonTitle>
+    </PageCommonWrap>
+  );
+};
+
+export default ProjectEntrust;
