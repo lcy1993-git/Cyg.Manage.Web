39bfe2091334fa5e70bfd6c2f443a96620d273c0
huangzhuo <darkmoon4195@qq.com>
2021-10-11 14:15:21 +0800

    feat(公司待办模块): 新增模块

---
+++ src/pages/project-management/project-entrust/components/entrust-table/index.tsx
@@ -0,0 +1,559 @@
+/* eslint-disable no-nested-ternary */
+import EmptyTip from '@/components/empty-tip';
+import {
+  AllProjectStatisticsParams,
+  applyKnot,
+  auditKnot,
+  getExternalArrangeStep,
+  getProjectInfo,
+  getProjectTableList,
+  getEngineerInfo,
+  againInherit,
+  deleteProject,
+} from '@/services/project-management/all-project';
+import { useGetButtonJurisdictionArray } from '@/utils/hooks';
+import { delay } from '@/utils/utils';
+import { useRequest, useSize } from 'ahooks';
+import { Menu, message, Modal, Popconfirm, Tooltip } from 'antd';
+import { Spin } from 'antd';
+import { Pagination } from 'antd';
+import { forwardRef, useImperativeHandle, Ref, useRef, useMemo, useState } from 'react';
+
+import EngineerTableItem, {
+  AddProjectValue,
+  TableItemCheckedInfo,
+} from '@/pages/project-management/all-project-new/components/engineer-table/engineer-table-item';
+import ScrollView from 'react-custom-scrollbars';
+import styles from './index.less';
+import CyTag from '@/components/cy-tag';
+import uuid from 'node-uuid';
+import { Dropdown } from 'antd';
+import { BarsOutlined, ExclamationCircleOutlined, LinkOutlined } from '@ant-design/icons';
+import { TableContext } from '@/pages/project-management/all-project-new/components/engineer-table/table-store';
+import EngineerDetailInfo from '@/pages/project-management/all-project-new/components/engineer-detail-info';
+import ProjectDetailInfo from '@/pages/project-management/all-project-new/components/project-detail-info';
+import moment from 'moment';
+import ImageIcon from '@/components/image-icon';
+
+const colorMap = {
+  立项: 'green',
+  委托: 'blue',
+  共享: 'yellow',
+  执行: 'yellow',
+};
+
+interface ExtractParams extends AllProjectStatisticsParams {
+  statisticalCategory?: string;
+}
+
+interface EntrustTableProps {
+  extractParams: ExtractParams;
+  onSelect?: (checkedValue: TableItemCheckedInfo[]) => void;
+  getStatisticsData?: (value: any) => void;
+  columnsConfig: string[];
+  finishEvent: () => void;
+  configFinishEvent?: (checkedValue: any) => void;
+}
+
+const EntrustTable = (props: EntrustTableProps, ref: Ref<any>) => {
+  const {
+    extractParams,
+    onSelect,
+    getStatisticsData,
+    columnsConfig = [],
+    finishEvent,
+    configFinishEvent,
+  } = props;
+  const [pageIndex, setPageIndex] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [tableSelectData, setTableSelectData] = useState<TableItemCheckedInfo[]>([]);
+
+  const [currentClickEngineerId, setCurrentClickEngineerId] = useState<string>('');
+
+  const [currentClickProjectId, setCurrentClickProjectId] = useState<string>('');
+  const [currentArrageProjectId, setCurrentArrageProjectId] = useState<string>('');
+
+  const [projectNeedInfo, setProjectNeedInfo] = useState({
+    engineerId: '',
+    areaId: '',
+    company: '',
+    companyName: '',
+  });
+  const [currentProName, setCurrentProName] = useState<string | undefined>('');
+  const [externalStepData, setExternalStepData] = useState<any>();
+
+  const [engineerModalVisible, setEngineerModalVisible] = useState<boolean>(false);
+  const [projectModalVisible, setProjectModalVisible] = useState<boolean>(false);
+
+  const { data: tableData, loading, run } = useRequest(getProjectTableList, { manual: true });
+
+  const scrollbar = useRef<any>(null);
+  const tableContentRef = useRef<HTMLDivElement>(null);
+
+  const tableContentSize = useSize(tableContentRef);
+
+  const buttonJurisdictionArray = useGetButtonJurisdictionArray();
+
+  const projectNameClickEvent = (engineerId: string) => {
+    setCurrentClickEngineerId(engineerId);
+    setEngineerModalVisible(true);
+  };
+
+  const refreshEvent = () => {
+    run({
+      ...extractParams,
+      pageIndex,
+      pageSize,
+    });
+    setTableSelectData([]);
+    onSelect?.([]);
+  };
+
+  const refreshByParams = (params: any) => {
+    run({
+      ...params,
+    });
+    setTableSelectData([]);
+    onSelect?.([]);
+  };
+
+  const delayRefresh = async () => {
+    await delay(500);
+    run({
+      ...extractParams,
+      pageIndex,
+      pageSize,
+    });
+    setTableSelectData([]);
+    onSelect?.([]);
+  };
+
+  const completeConfig = [
+    {
+      title: '项目名称',
+      dataIndex: 'name',
+      width: 300,
+      //   render: projectNameRender,
+      ellipsis: true,
+    },
+    {
+      title: '项目分类',
+      dataIndex: 'categoryText',
+      width: 100,
+      ellipsis: true,
+    },
+    {
+      title: '项目类型',
+      dataIndex: 'pTypeText',
+      width: 140,
+      ellipsis: true,
+    },
+    {
+      title: '电压等级',
+      dataIndex: 'kvLevelText',
+      width: 100,
+      ellipsis: true,
+    },
+    {
+      title: '项目性质',
+      dataIndex: 'natureTexts',
+      width: 180,
+      render: (record: any) => {
+        const { natureTexts = [] } = record;
+        return natureTexts.map((item: any) => {
+          return (
+            <CyTag key={uuid.v1()} className="mr7">
+              {item}
+            </CyTag>
+          );
+        });
+      },
+    },
+    {
+      title: '项目起止时间',
+      dataIndex: 'projectTime',
+      width: 190,
+      ellipsis: true,
+      render: (record: any) => {
+        const { startTime, endTime } = record;
+        if (startTime && endTime) {
+          return `${moment(startTime).format('YYYY-MM-DD')} 至 ${moment(endTime).format(
+            'YYYY-MM-DD',
+          )}`;
+        }
+        if (startTime && !endTime) {
+          return `开始时间: ${moment(startTime).format('YYYY-MM-DD')}`;
+        }
+        if (!startTime && endTime) {
+          return `截止时间: ${moment(startTime).format('YYYY-MM-DD')}`;
+        }
+        return '未设置起止时间';
+      },
+    },
+    {
+      title: '专业类别',
+      dataIndex: 'majorCategoryText',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '建设改造目的',
+      dataIndex: 'reformAimText',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '所属市公司',
+      dataIndex: 'cityCompany',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '所属县公司',
+      dataIndex: 'countyCompany',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '建设类型',
+      dataIndex: 'constructTypeText',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '项目类别',
+      dataIndex: 'pCategoryText',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '项目阶段',
+      dataIndex: 'stageText',
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: '项目属性',
+      dataIndex: 'pAttributeText',
+      width: 120,
+      ellipsis: true,
+    },
+
+    {
+      title: '勘察人',
+      dataIndex: 'surveyUser',
+      width: 120,
+      ellipsis: true,
+      render: (record: any) => {
+        return record.surveyUser ? `${record.surveyUser.value}` : '无需安排';
+      },
+    },
+    {
+      title: '设计人',
+      dataIndex: 'designUser',
+      width: 120,
+      ellipsis: true,
+      render: (record: any) => {
+        return record.designUser ? `${record.designUser.value}` : '';
+      },
+    },
+    {
+      title: '项目批次',
+      dataIndex: 'batchText',
+      width: 120,
+      ellipsis: true,
+    },
+  ];
+
+  const chooseColumns = useMemo(() => {
+    if (columnsConfig) {
+      return ['name', ...columnsConfig, 'sources', 'identitys', 'status', 'action'];
+    }
+    return [
+      'categoryText',
+      'kvLevelText',
+      'natureTexts',
+      'majorCategoryText',
+      'constructTypeText',
+      'stageText',
+      'exportCoordinate',
+      'surveyUser',
+      'designUser',
+      'identitys',
+    ];
+  }, [JSON.stringify(columnsConfig)]);
+
+  const columnsInfo = useMemo(() => {
+    const showColumns = completeConfig.filter((item) => chooseColumns.includes(item.dataIndex));
+    const columnsWidth = showColumns.reduce((sum, item) => {
+      return sum + (item.width ? item.width : 100);
+    }, 0);
+    const isOverflow = (tableContentSize.width ?? 0) - 40 - columnsWidth < 0;
+
+    if (scrollbar && scrollbar.current) {
+      // @ts-ignore
+      scrollbar.current.view.scroll({
+        left: 0,
+        behavior: 'auto',
+      });
+    }
+
+    return {
+      isOverflow,
+      columns: showColumns,
+      columnsWidth: columnsWidth + 38,
+    };
+  }, [chooseColumns, JSON.stringify(tableContentSize.width)]);
+
+  const tableResultData = useMemo(() => {
+    if (tableData) {
+      const { pagedData, statistics } = tableData;
+      const { items, pageIndex: resPageIndex, pageSize: resPageSize, total } = pagedData;
+      getStatisticsData?.(statistics);
+      return {
+        items: items ?? [],
+        pageIndex: resPageIndex,
+        pageSize: resPageSize,
+        total,
+        dataStartIndex: Math.floor((resPageIndex - 1) * pageSize + 1),
+        dataEndIndex: Math.floor((resPageIndex - 1) * pageSize + (items ?? []).length),
+        projectLen:
+          items
+            ?.filter((item: any) => item.projects && item.projects.length > 0)
+            .map((item: any) => item.projects)
+            .flat().length ?? 0,
+      };
+    }
+    return {
+      items: [],
+      pageIndex: 1,
+      pageSize: 20,
+      total: 0,
+      dataStartIndex: 0,
+      dataEndIndex: 0,
+      projectLen: 0,
+    };
+  }, [JSON.stringify(tableData)]);
+
+  const pageSizeChange = (page: any, size: any) => {
+    setPageIndex(1);
+    setPageSize(size);
+
+    run({
+      ...extractParams,
+      pageIndex: 1,
+      pageSize: size,
+    });
+    setTableSelectData([]);
+    onSelect?.([]);
+  };
+
+  // 列显示处理
+  const currentPageChange = (page: any, size: any) => {
+    // 判断当前page是否改变, 没有改变代表是change页面触发
+    if (pageSize === size) {
+      setPageIndex(page === 0 ? 1 : page);
+
+      run({
+        ...extractParams,
+        pageIndex: page,
+        pageSize,
+      });
+      setTableSelectData([]);
+      onSelect?.([]);
+    }
+  };
+
+  const tableItemSelectEvent = (projectSelectInfo: TableItemCheckedInfo) => {
+    console.log(projectSelectInfo);
+    // 监测现在数组是否含有此id的数据
+    const hasData = tableSelectData.findIndex(
+      (item) => item.projectInfo.id === projectSelectInfo.projectInfo.id,
+    );
+    const copyData: TableItemCheckedInfo[] = JSON.parse(JSON.stringify(tableSelectData));
+    if (hasData > -1) {
+      copyData.splice(hasData, 1, projectSelectInfo);
+      setTableSelectData(copyData);
+      onSelect?.(copyData);
+    } else {
+      // 代表没有数据，那就直接插进去
+      setTableSelectData([...tableSelectData, projectSelectInfo]);
+      onSelect?.([...tableSelectData, projectSelectInfo]);
+    }
+  };
+
+  const engineerTableElement = tableResultData?.items.map((item: any) => {
+    return (
+      <EngineerTableItem
+        getClickProjectId={projectNameClickEvent}
+        onChange={tableItemSelectEvent}
+        columns={columnsInfo.columns}
+        key={item.id}
+        projectInfo={item}
+        isOverflow={columnsInfo.isOverflow}
+        columnsWidth={columnsInfo.columnsWidth}
+        contentWidth={tableContentSize.width!}
+      />
+    );
+  });
+
+  useImperativeHandle(ref, () => ({
+    // changeVal 就是暴露给父组件的方法
+    refresh: () => {
+      run({
+        ...extractParams,
+        pageIndex,
+        pageSize,
+      });
+      setTableSelectData([]);
+      onSelect?.([]);
+    },
+    search: () => {
+      setPageIndex(1);
+      run({
+        ...extractParams,
+        pageIndex: 1,
+        pageSize,
+      });
+      if (scrollbar && scrollbar.current) {
+        // @ts-ignore
+        scrollbar.current.view.scroll({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }
+      setTableSelectData([]);
+      onSelect?.([]);
+    },
+    searchByParams: (params: object) => {
+      setPageIndex(1);
+      run({
+        ...params,
+        pageIndex: 1,
+        pageSize,
+      });
+      if (scrollbar && scrollbar.current) {
+        scrollbar.current.view.scroll({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }
+      setTableSelectData([]);
+      onSelect?.([]);
+    },
+    delayRefresh: async (ms: number) => {
+      await delay(500);
+      run({
+        ...extractParams,
+        pageIndex,
+        pageSize,
+      });
+      setTableSelectData([]);
+      onSelect?.([]);
+    },
+  }));
+
+  const scrollEvent = (size: any) => {
+    if (size) {
+      const tableTitle = document.getElementsByClassName('tableTitleContent');
+      const tableCheckbox = document.getElementsByClassName('checkboxContent');
+      const tableNameTd = document.getElementsByClassName('nameTdContent');
+      const tableActionTd = document.getElementsByClassName('actionTdContent');
+      const tableStatusTd = document.getElementsByClassName('statusTdContent');
+      if (tableTitle && tableTitle.length > 0) {
+        for (let i = 0; i < tableTitle.length; i += 1) {
+          // @ts-ignore
+          tableTitle[i].style.left = `${size.scrollLeft}px`;
+        }
+      }
+      if (tableCheckbox && tableCheckbox.length > 0) {
+        for (let i = 0; i < tableCheckbox.length; i += 1) {
+          // @ts-ignore
+          tableCheckbox[i].style.left = `${size.scrollLeft}px`;
+        }
+      }
+      if (tableNameTd && tableNameTd.length > 0) {
+        for (let i = 0; i < tableNameTd.length; i += 1) {
+          // @ts-ignore
+          tableNameTd[i].style.left = `${size.scrollLeft + 38}px`;
+        }
+      }
+      if (tableActionTd && tableActionTd.length > 0) {
+        for (let i = 0; i < tableActionTd.length; i += 1) {
+          // @ts-ignore
+          tableActionTd[i].style.left = `${size.scrollLeft + tableContentSize.width - 60}px`;
+        }
+      }
+      if (tableStatusTd && tableStatusTd.length > 0) {
+        for (let i = 0; i < tableStatusTd.length; i += 1) {
+          // @ts-ignore
+          tableStatusTd[i].style.left = `${size.scrollLeft + tableContentSize.width - 180}px`;
+        }
+      }
+    }
+  };
+
+  const scrollBarRenderView = (params: any) => {
+    const { style, ...rest } = params;
+    const viewStyle = {
+      backgroundColor: `rgba(0, 0, 0, 0.2)`,
+      borderRadius: '6px',
+      cursor: 'pointer',
+      zIndex: 100,
+    };
+    return <div style={{ ...style, ...viewStyle }} {...rest} />;
+  };
+
+  return (
+    <TableContext.Provider
+      value={{
+        tableSelectData,
+        setTableSelectData,
+      }}
+    >
+      <div className={styles.engineerTable}>
+        <div className={styles.engineerTableContent} ref={tableContentRef}>
+          <ScrollView
+            ref={scrollbar}
+            onUpdate={scrollEvent}
+            renderThumbHorizontal={scrollBarRenderView}
+            renderThumbVertical={scrollBarRenderView}
+          >
+            <Spin spinning={loading}>
+              {tableResultData.items.length > 0 && engineerTableElement}
+              {tableResultData.items.length === 0 && <EmptyTip className="pt20" />}
+            </Spin>
+          </ScrollView>
+        </div>
+        <div className={styles.engineerTablePaging}>
+          <div className={styles.engineerTablePagingLeft}>
+            <span>显示第</span>
+            <span className={styles.importantTip}>{tableResultData.dataStartIndex}</span>
+            <span>到第</span>
+            <span className={styles.importantTip}>{tableResultData.dataEndIndex}</span>
+            <span>条记录，总共</span>
+            <span className={styles.importantTip}>{tableResultData.items.length}</span>
+            <span>个工程，</span>
+            <span className={styles.importantTip}>{tableResultData.projectLen}</span>个项目
+          </div>
+          <div className={styles.engineerTablePagingRight}>
+            <Pagination
+              pageSize={pageSize}
+              onChange={currentPageChange}
+              size="small"
+              total={tableResultData.total}
+              current={pageIndex}
+              // hideOnSinglePage={true}
+              showSizeChanger
+              showQuickJumper
+              onShowSizeChange={pageSizeChange}
+              style={{ display: 'inline-flex', paddingRight: '25px' }}
+            />
+          </div>
+        </div>
+      </div>
+    </TableContext.Provider>
+  );
+};
+
+export default forwardRef(EntrustTable);
--- src/pages/project-management/project-entrust/index.tsx
+++ src/pages/project-management/project-entrust/index.tsx
@@ -1,11 +1,84 @@
 import CommonTitle from '@/components/common-title';
 import PageCommonWrap from '@/components/page-common-wrap';
-import React from 'react';
-
+import TableSearch from '@/components/table-search';
+import EntrustTable from './components/entrust-table';
+import { Button, Input } from 'antd';
+import styles from './index.less';
+import React, { useState } from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import FilterEntrustModal from './components/filter-entrust-modal';
+const { Search } = Input;
 const ProjectEntrust: React.FC = () => {
+  const [keyWord, setKeyWord] = useState<string>();
+  const [screenModalVisible, setScreenModalVisible] = useState(false);
+  const [searchParams, setSearchParams] = useState({
+    category: [],
+    stage: [],
+    constructType: [],
+    nature: [],
+    kvLevel: [],
+    status: [],
+    majorCategory: [],
+    pType: [],
+    reformAim: [],
+    classification: [],
+    attribute: [],
+    sourceType: [],
+    identityType: [],
+    areaType: '-1',
+    areaId: '',
+    dataSourceType: [],
+    logicRelation: 2,
+    startTime: '',
+    endTime: '',
+    designUser: '',
+    surveyUser: '',
+  });
+  const searchEvent = () => {};
+
+  const screenClickEvent = (params: any) => {
+    // setSearchParams({ ...params, keyWord, statisticalCategory });
+    // searchByParams({ ...params, engineerFavoritesId: selectedFavId, keyWord, statisticalCategory });
+  };
+
   return (
     <PageCommonWrap>
-      <CommonTitle></CommonTitle>
+      <CommonTitle>待办项目</CommonTitle>
+      <div>
+        在当前列表中可以查看所属公司被其他单位委托的项目，并且可以将该项目获取至当前个人账号，获取后的项目在【我的工作台】模块中查看。
+      </div>
+      <div className={styles.searchAndButton}>
+        <div className={styles.searchProject}>
+          <TableSearch className="mr22" label="" width="300px">
+            <Search
+              placeholder="请输入工程/项目名称"
+              enterButton
+              value={keyWord}
+              onChange={(e) => setKeyWord(e.target.value)}
+              onSearch={() => searchEvent()}
+            />
+          </TableSearch>
+          <Button onClick={() => setScreenModalVisible(true)}>筛选</Button>
+        </div>
+
+        {/* {(buttonJurisdictionArray?.includes('all-project-project-approval')  */}
+
+        <Button className="mr7" type="primary" style={{ width: '70px', borderRadius: '5px' }}>
+          获取
+        </Button>
+
+        {/* )} */}
+      </div>
+      <div>
+        <EntrustTable />
+      </div>
+
+      <FilterEntrustModal
+        visible={screenModalVisible}
+        onChange={setScreenModalVisible}
+        finishEvent={screenClickEvent}
+        searchParams={searchParams}
+      />
     </PageCommonWrap>
   );
 };
