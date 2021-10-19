ea4f23b7d7334f544c4a323804947c29cefc988e
psj <271533323@qq.com>
2021-10-19 17:55:55 +0800

    fix: 修改email部分bug

--- src/pages/index/test-page/index.tsx
+++ src/pages/index/test-page/index.tsx
@@ -1,11 +1,10 @@
-import { divide } from "@umijs/deps/compiled/lodash"
-import VirtualScroll from "./components/virtual-scroll"
+import FilePDFReact from "./file-pdf-react"
+
 
 const Test = () => {
-  const data = Array(100).fill(1)
-  console.log(data);
+
   
-  return <VirtualScroll data={data} component={() => <div style={{height: 30, border: "1px solid red"}}>123</div>}/>
+  return <FilePDFReact />
 }
 
 export default Test
