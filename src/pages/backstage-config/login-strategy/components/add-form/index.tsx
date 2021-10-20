9d148bdfff56c3faca8beb7776627535f7eee40e
huangzhuo <darkmoon4195@qq.com>
2021-10-20 14:57:35 +0800

    feat(调整): 添加错误码选项

--- src/pages/backstage-config/login-strategy/components/add-form/index.tsx
+++ src/pages/backstage-config/login-strategy/components/add-form/index.tsx
@@ -6,15 +6,16 @@ import EnumSelect from '@/components/enum-select';
 export enum LoginEnum {
   '授权账号' = 1,
   '授权IP ',
+  '错误码授权',
 }
 
 const AddLoginStrategyForm: React.FC = () => {
   return (
     <>
       <CyFormItem
-        labelWidth={115}
+        labelWidth={160}
         // align="right"
-        label="授权账号/授权IP"
+        label="授权账号/授权IP/错误码"
         name="key"
         required
         rules={[{ required: true, message: '请输入授权账号或IP' }]}
@@ -23,8 +24,8 @@ const AddLoginStrategyForm: React.FC = () => {
       </CyFormItem>
 
       <CyFormItem
-        labelWidth={115}
-        // align="right"
+        labelWidth={160}
+        align="right"
         label="类型"
         name="authorizeType"
         required
@@ -32,7 +33,7 @@ const AddLoginStrategyForm: React.FC = () => {
       >
         <EnumSelect placeholder="请选择" enumList={LoginEnum} />
       </CyFormItem>
-      <CyFormItem labelWidth={115} label="备注" name="remark">
+      <CyFormItem align="right" labelWidth={160} label="备注" name="remark">
         <Input.TextArea placeholder="请输入备注" showCount />
       </CyFormItem>
     </>
