6fa6f38101f2e61b9a27aa4d6b917ce522920436
yangl <517498696@qq.com>
2021-11-20 20:02:45 +0800

    feat(我的工作台): 我的工作台重构

--- src/components/table-export-button/index.tsx
+++ src/components/table-export-button/index.tsx
@@ -1,73 +1,77 @@
-import React from 'react';
-import { Menu, Button, Dropdown, message } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import { commonExport } from '@/services/common';
+import { commonExport } from '@/services/common'
+import { DownOutlined } from '@ant-design/icons'
+import { Button, Dropdown, Menu, message } from 'antd'
+import React from 'react'
 
 interface TableExportButtonProps {
-  selectIds?: string[];
-  exportUrl: string;
-  extraParams?: object;
-  fileName?: string;
-  selectSlot?: () => React.ReactNode;
+  selectIds?: string[]
+  exportUrl: string
+  extraParams?: object
+  fileName?: string
+  selectSlot?: () => React.ReactNode
 }
 
 const TableExportButton: React.FC<TableExportButtonProps> = (props) => {
-  const { selectIds = [], exportUrl = '', extraParams, fileName = '表格', selectSlot } = props;
+  const { selectIds = [], exportUrl = '', extraParams, fileName = '表格', selectSlot } = props
   const exportChoosedRow = async () => {
     if (selectIds && selectIds.length === 0) {
-      message.error('请选择需要导出的行');
-      return;
+      message.error('请选择需要导出的行')
+      return
     }
-    const res = await commonExport(exportUrl, extraParams, selectIds);
+    const res = await commonExport(exportUrl, extraParams, selectIds)
     let blob = new Blob([res], {
       type: 'application/vnd.ms-excel;charset=utf-8',
-    });
-    let finalyFileName = `${fileName}.xlsx`;
+    })
+    let finalyFileName = `${fileName}.xlsx`
     // for IE
     if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, finalyFileName);
+      window.navigator.msSaveOrOpenBlob(blob, finalyFileName)
     } else {
       // for Non-IE
-      let objectUrl = URL.createObjectURL(blob);
-      let link = document.createElement('a');
-      link.href = objectUrl;
-      link.setAttribute('download', finalyFileName);
-      document.body.appendChild(link);
-      link.click();
-      window.URL.revokeObjectURL(link.href);
+      let objectUrl = URL.createObjectURL(blob)
+      let link = document.createElement('a')
+      link.href = objectUrl
+      link.setAttribute('download', finalyFileName)
+      document.body.appendChild(link)
+      link.click()
+      window.URL.revokeObjectURL(link.href)
     }
-    message.success('导出成功');
-  };
+    message.success('导出成功')
+  }
 
   const exportAllRow = async () => {
-    const res = await commonExport(exportUrl, extraParams, []);
+    const res = await commonExport(exportUrl, extraParams, [])
     let blob = new Blob([res], {
       type: 'application/vnd.ms-excel;charset=utf-8',
-    });
-    let finalyFileName = `${fileName}.xlsx`;
+    })
+    let finalyFileName = `${fileName}.xlsx`
     // for IE
     if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, finalyFileName);
+      window.navigator.msSaveOrOpenBlob(blob, finalyFileName)
     } else {
       // for Non-IE
-      let objectUrl = URL.createObjectURL(blob);
-      let link = document.createElement('a');
-      link.href = objectUrl;
-      link.setAttribute('download', finalyFileName);
-      document.body.appendChild(link);
-      link.click();
-      window.URL.revokeObjectURL(link.href);
+      let objectUrl = URL.createObjectURL(blob)
+      let link = document.createElement('a')
+      link.href = objectUrl
+      link.setAttribute('download', finalyFileName)
+      document.body.appendChild(link)
+      link.click()
+      window.URL.revokeObjectURL(link.href)
     }
-    message.success('导出成功');
-  };
+    message.success('导出成功')
+  }
 
   const importButoonMenu = (
     <Menu>
-      <Menu.Item onClick={() => exportChoosedRow()}>导出所选</Menu.Item>
-      <Menu.Item onClick={() => exportAllRow()}>导出所有</Menu.Item>
+      <Menu.Item key="exportPart" onClick={() => exportChoosedRow()}>
+        导出所选
+      </Menu.Item>
+      <Menu.Item key="exportAll" onClick={() => exportAllRow()}>
+        导出所有
+      </Menu.Item>
       {selectSlot ? <Menu.Item>{selectSlot?.()}</Menu.Item> : null}
     </Menu>
-  );
+  )
 
   return (
     <>
@@ -77,7 +81,7 @@ const TableExportButton: React.FC<TableExportButtonProps> = (props) => {
         </Button>
       </Dropdown>
     </>
-  );
-};
+  )
+}
 
-export default TableExportButton;
+export default TableExportButton
