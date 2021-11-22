8caec46c8c0a601b888c04675ef6a06f92878af0
fenghan <fenghan770@gmail.com>
2021-11-22 18:00:16 +0800

    fix: 恢复 layouts 文件

---
+++ src/layouts/context.tsx
@@ -0,0 +1,25 @@
+import { createContext, SetStateAction, useContext } from 'react'
+
+interface LayoutContextValue {
+  resourceManageFlag?: boolean
+  workHandoverFlag?: boolean
+  clearAgainLogin?: () => void
+  mapSelectCity?: string
+  allProjectSearchProjectId?: string
+  allProjectSearchParams?: any
+  setResourceManageFlag?: (value: boolean) => void
+  setMapSelectCity?: (value: string) => void
+  setWorkHandoverFlag?: (value: boolean) => void
+  setAllProjectSearchProjectId?: (value: string) => void
+  setAllProjectSearchParams?: (value?: any) => void
+  removeTab?: (value: string) => void
+  /** 预设计 */
+  preDesignItem?: any
+  setPreDesignItem: React.Dispatch<SetStateAction<any>>
+}
+
+const LayoutContext = createContext({} as LayoutContextValue)
+
+export const LayoutProvider = LayoutContext.Provider
+
+export const useLayoutStore = () => useContext(LayoutContext)
