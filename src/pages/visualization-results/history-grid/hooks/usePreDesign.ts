08d14af61dadbc48090124801477a4282d8ee6bb
fenghan <fenghan770@gmail.com>
2021-12-02 12:57:48 +0800

    feat: 预设计初始化竞态判断

--- src/pages/visualization-results/history-grid/hooks/usePreDesign.ts
+++ src/pages/visualization-results/history-grid/hooks/usePreDesign.ts
@@ -16,16 +16,22 @@ export const usePreDesign = (dispatch: HistoryDispatch) => {
 
   /** 初始化 */
   const init = useCallback(
-    async (preDesignItemData: HistoryState['preDesignItemData']) => {
+    async (preDesignItemData: HistoryState['preDesignItemData'], flag: { cancel: boolean }) => {
       dispatch({ type: 'reset', payload: { ...INITIAL_STATE, mode: gridType, preDesignItemData } })
 
       try {
         const { content } = await initPreDesign(preDesignItemData.id)
 
+        // 出现竞态，取消状态修改
+        if (flag.cancel) return
+
         dispatch({
           type: 'changePreDesignDataSource',
           payload: { ...INITIAL_DATA_SOURCE, id: content },
         })
+
+        // eslint-disable-next-line no-console
+        console.log('预设计初始化')
       } catch (e: any) {
         message.error(e?.message || '初始化预设计失败，请重试')
       }
@@ -34,13 +40,19 @@ export const usePreDesign = (dispatch: HistoryDispatch) => {
   )
 
   useEffect(() => {
+    // 是否执行了初始化
+    let executedInit = false
+    // 标记，用以当出现竞态时，取消上一次初始化函数中的修改状态操作
+    let flag = { cancel: false }
+
     if (gridType === 'preDesign') {
       let preDesignItemPayload
 
       if (preDesignItem?.id) {
+        // 从我的工作台项目进入
         preDesignItemPayload = preDesignItem
       } else {
-        // 解决刷新获取不到项目数据
+        // 刷新页面
         preDesignItemPayload = JSON.parse(localStorage.getItem('preDesignItem') || 'null')
       }
 
@@ -48,8 +60,17 @@ export const usePreDesign = (dispatch: HistoryDispatch) => {
       const lastItem = lastPreDesignItem.current
 
       if (!lastItem || (preDesignItemPayload && lastItem.id !== preDesignItemPayload.id)) {
-        // * 首次打开 | 刷新页面 | 从不同项目再次进入 => 初始化
-        init(preDesignItemPayload)
+        // * (首次从我的工作台项目进入 | 后续从不同项目再次进入 | 刷新页面 ) => 初始化
+
+        executedInit = true
+        init(preDesignItemPayload, flag)
+      }
+    }
+
+    return () => {
+      if (executedInit) {
+        // 如果上次 effect 中执行了初始化
+        flag.cancel = true
       }
     }
   }, [preDesignItem, dispatch, gridType, init])
