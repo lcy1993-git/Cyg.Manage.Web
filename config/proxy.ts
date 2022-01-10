07d6e915f826fe6650b7ada423f678667be70d78
huangzhuo <darkmoon4195@qq.com>
2022-01-10 17:30:31 +0800

    fix(修复评审详情bug): 时间倒序排列

--- config/proxy.ts
+++ config/proxy.ts
@@ -11,9 +11,9 @@ export default {
     '/api/': {
       // http://10.6.1.54:21525/api/
       // 171.223.214.154:21573
-      // target: 'https://srthkf2.gczhyun.com:21530',
-      // target: 'https://srthcs2.gczhyun.com:21564',
-      target: 'https://srthk8scs2.gczhyun.com:21530',
+      target: 'https://srthkf2.gczhyun.com:21530',
+      // target: 'https://srthcs2.gczhyun.com:21574',
+      // target: 'https://srthk8scs2.gczhyun.com:21530',
       // 测试服 1
       // target: 'http://171.223.214.154:21563',
       // 测试服 2
