ea7e40c0506ced7ffc21047d09e0c31cd1ff345f
lengzw <lengzw@sznari.com>
2022-02-15 16:29:36 +0800

    feat: 调试业务看板接口

--- src/pages/backstage-config/business-kanban/index.tsx
+++ src/pages/backstage-config/business-kanban/index.tsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import PageCommonWrap from '@/components/page-common-wrap'
 import styles from './index.less'
 import { Progress, Select, Table, Tooltip } from 'antd'
 import { LeftOutlined, QuestionCircleOutlined } from '@ant-design/icons'
 import GeneralTable from '@/components/general-table'
+import moment, {Moment} from "moment";
 
 const BusinessBoard: React.FC = () => {
-  const [value, setValue] = useState<string>('project')
+  const [statisticalType, setStatisticalType] = useState<string>('1')
   const [detail, setDetail] = useState<boolean>(false)
-  const showDetail = () => {
-    setDetail(true)
+  const [id,setId] = useState('');
+  const showDetail = (row: { companyId: React.SetStateAction<string> }) => {
+    setId(row.companyId)
     setDetail(true)
   }
+  const tableRef = React.useRef<HTMLDivElement>(null);
+  const detailTableRef = React.useRef<HTMLDivElement>(null);
   const columns = [
     {
       title: '公司名称',
-      dataIndex: 'name',
-      key: 'name',
+      dataIndex: 'companyName',
+      key: 'companyName',
       render: (text: string, row: any) => {
         return (
-          <span onClick={() => showDetail()} className={styles.tableLink}>
+          <span onClick={() => showDetail(row)} className={styles.tableLink}>
             {text}
           </span>
         )
@@ -27,40 +31,41 @@ const BusinessBoard: React.FC = () => {
     },
     {
       title: '立项',
-      dataIndex: 'age',
-      key: 'age',
+      dataIndex: 'establishmentQuntity',
+      key: 'establishmentQuntity',
     },
     {
       title: '勘察',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'surveyQuntity',
+      key: 'surveyQuntity',
     },
     {
       title: '设计',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'designQuntity',
+      key: 'designQuntity',
     },
     {
       title: '造价',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'economyQuntity',
+      key: 'economyQuntity',
     },
     {
       title: '评审',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'reviewQuntity',
+      key: 'reviewQuntity',
     },
     {
       title: '结项',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'closeQuntity',
+      key: 'closeQuntity',
     },
     {
       title: '合计',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'totalQuntity',
+      key: 'totalQuntity',
     },
     {
+      width: 300,
       title: () => {
         return (
           <span>
@@ -71,90 +76,93 @@ const BusinessBoard: React.FC = () => {
           </span>
         )
       },
-      dataIndex: 'address',
-      key: 'address',
-      render: () => {
-        return <Progress percent={30} />
+      dataIndex: 'progress',
+      key: 'progress',
+      render: (progress:number) => {
+        return <div style={{
+          padding: '0 20px 00 10px'
+        }}>
+          <Progress percent={Number(progress.toFixed(0))} />
+        </div>
       },
     },
     {
       title: '最近操作时间',
-      dataIndex: 'address',
-      key: 'address',
+      width:150,
+      dataIndex: 'lastUpdateTime',
+      key: 'lastUpdateTime',
+      render: (time:Moment)=>{
+        return moment(time).format('YYYY-MM-DD')
+      }
     },
   ]
   const detailColumns = [
     {
       title: '序号',
-      dataIndex: 'name',
-      key: 'name',
+      width: 50,
+      index: 'number',
+      render: (text: any, record: any, index: number) => `${index + 1}`,
+      align: 'center' as 'center',
     },
     {
       title: '项目名称',
-      dataIndex: 'age',
-      key: 'age',
+      dataIndex: 'projectName',
+      key: 'projectName',
     },
     {
       title: '立项/执行公司',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'companyName',
+      key: 'companyName',
     },
     {
       title: '计划天数',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'planDays',
+      key: 'planDays',
+      render: (day:number)=>{
+        return <span>{day}天</span>
+      }
     },
     {
       title: '当前阶段',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'statusText',
+      key: 'statusText',
     },
     {
       title: '项目进度',
-      dataIndex: 'address',
-      key: 'address',
-      render: () => {
-        return <Progress percent={30} />
+      dataIndex: 'progress',
+      key: 'progress',
+      render: (progress:number) => {
+        return <div style={{
+          padding: '0 20px 00 10px'
+        }}>
+          <Progress percent={Number(progress.toFixed(0))} />
+        </div>
       },
     },
     {
       title: '超期情况',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'overdueDays',
+      key: 'overdueDays',
+      render: (day:number)=>{
+        return <span>已逾期{day}天</span>
+      }
     },
     {
       title: '最近操作时间',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'lastUpdateTime',
+      key: 'lastUpdateTime',
+      render: (time:Moment)=>{
+        return moment(time).format('YYYY-MM-DD')
+      }
     },
   ]
-  const data = [
-    {
-      key: '1',
-      id: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 1,
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      id: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 2,
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      id: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 3,
-      tags: ['cool', 'teacher'],
-    },
-  ]
-  const rowSummary = (pageData: any[] | readonly object[], name: string) => {
+  const reset = () => {
+    if (tableRef && tableRef.current) {
+      // @ts-ignore
+      tableRef.current.search();
+    }
+  }
+  const rowSummary = (pageData: any[] | readonly object[] , name: string) => {
     // @ts-ignore
     return pageData?.reduce((total: number, next: { [x: string]: any }) => {
       if (isNaN(total)) {
@@ -162,24 +170,38 @@ const BusinessBoard: React.FC = () => {
       } else {
         return Number(total) + Number(next[name])
       }
-    })
+    },0)
   }
+  useEffect(() => {
+    reset()
+  },[statisticalType])
+  useEffect(()=>{
+    if (detail && detailTableRef && detailTableRef.current){
+      // @ts-ignore
+      detailTableRef.current.search()
+    } else {
+      setId('')
+      if (tableRef && tableRef.current){
+        // @ts-ignore
+        tableRef.current.refresh()
+      }
+    }
+  },[detail])
   return (
     <PageCommonWrap noPadding>
       <div className={styles.businessBoard}>
-        {value}
         <div className={styles.businessBoardTitle}>业务看板</div>
         <br />
         <div>
           统计模式 :&nbsp;&nbsp;
           <Select
-            defaultValue="project"
+            defaultValue="1"
             style={{ width: 200 }}
             disabled={detail}
-            onSelect={(val) => setValue(val)}
+            onSelect={(val) => setStatisticalType(val)}
           >
-            <Select.Option value="project">立项统计</Select.Option>
-            <Select.Option value="execution">执行统计</Select.Option>
+            <Select.Option value="1">立项统计</Select.Option>
+            <Select.Option value="2">执行统计</Select.Option>
           </Select>
         </div>
         {detail ? (
@@ -190,14 +212,27 @@ const BusinessBoard: React.FC = () => {
           <div className={styles.tableTitle}>综合进度</div>
         )}
         <div>
-          {detail ? (
-            <GeneralTable rowKey={'id'} columns={detailColumns} url={''} dataSource={data} />
-          ) : (
+          {detail  &&
             <GeneralTable
-              rowKey={'id'}
+              ref={detailTableRef}
+              rowKey={'companyId'}
+              columns={detailColumns}
+              extractParams={{
+                statisticalType:Number(statisticalType),
+                companyId:id
+              }}
+              url={'/BusinessBoard/GetProjectProgress'}
+               />
+          }
+          {
+            !detail && <GeneralTable
+              ref={tableRef}
+              rowKey={'companyId'}
               columns={columns}
-              url={''}
-              dataSource={data}
+              url={'/BusinessBoard/GetSyntheticalProgress'}
+              extractParams={{
+                statisticalType:Number(statisticalType)
+              }}
               summary={(pageData) => {
                 return (
                   <>
@@ -205,36 +240,34 @@ const BusinessBoard: React.FC = () => {
                       <Table.Summary.Cell index={0}>合计</Table.Summary.Cell>
                       <Table.Summary.Cell index={1}>-</Table.Summary.Cell>
                       <Table.Summary.Cell index={2}>
-                        {rowSummary(pageData, 'age')}
+                        {rowSummary(pageData, 'establishmentQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={3}>
-                        {rowSummary(pageData, 'address')}
+                        {rowSummary(pageData, 'surveyQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={4}>
-                        {rowSummary(pageData, 'address')}
+                        {rowSummary(pageData, 'designQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={5}>
-                        {rowSummary(pageData, 'address')}
+                        {rowSummary(pageData, 'economyQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={6}>
-                        {rowSummary(pageData, 'address')}
+                        {rowSummary(pageData, 'reviewQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={7}>
-                        {rowSummary(pageData, 'address')}
+                        {rowSummary(pageData, 'closeQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={8}>
-                        {rowSummary(pageData, 'address')}
+                        {rowSummary(pageData, 'totalQuntity')}
                       </Table.Summary.Cell>
                       <Table.Summary.Cell index={9}>-</Table.Summary.Cell>
-                      <Table.Summary.Cell index={10}>
-                        {rowSummary(pageData, 'address')}
-                      </Table.Summary.Cell>
+                      <Table.Summary.Cell index={10}>-</Table.Summary.Cell>
                     </Table.Summary.Row>
                   </>
                 )
               }}
             />
-          )}
+          }
         </div>
       </div>
     </PageCommonWrap>
