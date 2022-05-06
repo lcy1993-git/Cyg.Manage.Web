6228a8f4292832b8c228367b4a1dc9456e3269b3
huangzhuo <darkmoon4195@qq.com>
2022-05-06 17:30:17 +0800

    fix(1.0.212): 修复长园服演示服删除模态框样式问题；调试word预览；

--- src/pages/index/test-page/index.tsx
+++ src/pages/index/test-page/index.tsx
@@ -1,10 +1,31 @@
 import PageCommonWrap from '@/components/page-common-wrap'
-import MyProject from '@/pages/project-management/my-work/components/my-project'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import FileDocxView from '@/components/api-file-view/componnents/file-docx-view'
+import { downLoadFileItem } from '@/services/operation-config/company-file'
+import { Modal } from 'antd'
 const Test = () => {
+  const [data, setData] = useState(null)
+  const [visible, setVisible] = useState<boolean>(true)
+  useEffect(() => {
+    downLoadFileItem({ fileId: '1522494038355251200' }).then((res) => {
+      setData(res)
+    })
+  }, [])
+
   return (
     <PageCommonWrap noPadding>
-      <MyProject />
+      <Modal
+        maskClosable={false}
+        title="项目成果"
+        width="90%"
+        destroyOnClose
+        onCancel={() => setVisible(false)}
+        visible={visible}
+        bodyStyle={{ padding: '60px', overflowY: 'auto', height: '750px' }}
+        footer={null}
+      >
+        <FileDocxView data={data} />
+      </Modal>
     </PageCommonWrap>
   )
 }
