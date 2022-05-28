4f1bd7b9b6dca77b95e71abe782ffd1a4e462ea7
Licy <62718959@qq.com>
2022-05-28 19:49:00 +0800

    feat: 网架管理, 绘制线路

--- src/pages/visualization-results/grid-manage/GridMap/utils/style.ts
+++ src/pages/visualization-results/grid-manage/GridMap/utils/style.ts
@@ -17,17 +17,17 @@ export const pointStyle = (data: any, selected: boolean = false) => {
     font = 'gridManageIconfont'
   let color = selected ? `rgba(110, 74, 192, 1)` : `rgba(8,210,42,1)`
   switch (data.kvLevel) {
-    case 5:
+    case 7:
       color = `rgba(8,210,42,1)`
       break
-    case 4:
+    case 6:
       color = `rgba(64,56,31,1)`
       break
-    case 3:
-    case 1:
+    case 5:
+    case 4:
       color = `rgba(170,170,85,1)`
       break
-    case 0:
+    case 3:
       color = `rgba(212,212,0,1)`
       break
     default:
@@ -44,13 +44,13 @@ export const pointStyle = (data: any, selected: boolean = false) => {
       else text = '\ue11a'
       break
     case TRANSFORMERSUBSTATION: // 变电站
-      if (data.kvLevel === 5)
+      if (data.kvLevel === 7)
         // 330KV
         text = '\ue12a'
-      else if (data.kvLevel === 4)
+      else if (data.kvLevel === 6)
         // 110KV
         text = '\ue127'
-      else if (data.kvLevel === 1 || data.kvLevel === 3)
+      else if (data.kvLevel === 5 || data.kvLevel === 4)
         // 35KV/20KV
         text = '\ue128'
       break
@@ -86,7 +86,7 @@ export const pointStyle = (data: any, selected: boolean = false) => {
   }
   return new Style({
     text: new Text({
-      font: 'Normal 52px ' + font,
+      font: 'Normal 28px ' + font,
       text,
       fill: new Fill({
         color: color,
