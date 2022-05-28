692313e4051da8347d686d0abb8e546af2d8bf9a
Licy <62718959@qq.com>
2022-05-28 16:41:15 +0800

    feat: 网架管理, 绘制线路

--- src/pages/visualization-results/grid-manage/GridMap/utils/style.ts
+++ src/pages/visualization-results/grid-manage/GridMap/utils/style.ts
@@ -13,42 +13,72 @@ import {
   TRANSFORMERSUBSTATION,
 } from '../../DrawToolbar/GridUtils'
 export const pointStyle = (data: any, selected: boolean = false) => {
-  let text
-  let color = selected ? `rgba(8,210,42,1)` : `rgba(145, 145, 255, 1)`
+  let text,
+    font = 'webgisIconFont'
+  let color = selected ? `rgba(110, 74, 192, 1)` : `rgba(8,210,42,1)`
+  switch (data.kvLevel) {
+    case 5:
+      color = `rgba(8,210,42,1)`
+      break
+    case 4:
+      color = `rgba(64,56,31,1)`
+      break
+    case 3:
+    case 1:
+      color = `rgba(170,170,85,1)`
+      break
+    case 0:
+      color = `rgba(212,212,0,1)`
+      break
+    default:
+      break
+  }
   // 根据点位的类型设置图符
   switch (data.featureType) {
-    case POWERSUPPLY:
-      text = '\ue86d'
+    case POWERSUPPLY: // 电源
+      if (data.powerType === '水电') text = '\ue11a'
+      else if (data.powerType === '火电') text = '\ue124'
+      else if (data.powerType === '风电') text = '\ue11a'
+      else if (data.powerType === '光伏') text = '\ue119'
+      else if (data.powerType === '生物质能') text = '\ue119'
+      else text = '\ue11a'
       break
-    case TRANSFORMERSUBSTATION:
-      text = '\ue86d'
+    case TRANSFORMERSUBSTATION: // 变电站
+      if (data.kvLevel === 5)
+        // 330KV
+        text = '\ue12a'
+      else if (data.kvLevel === 4)
+        // 110KV
+        text = '\ue127'
+      else if (data.kvLevel === 1 || data.kvLevel === 3)
+        // 35KV/20KV
+        text = '\ue128'
       break
-    case CABLEWELL:
-      text = '\ue869'
+
+    case CABLEWELL: /* 电缆井 */
+    case TOWER /* 杆塔 */:
+      text = '\ue119'
       break
-    case TOWER:
-      text = '\ue870'
+    case BOXTRANSFORMER /** 箱变 **/:
+      text = '\ue11c'
       break
-    case BOXTRANSFORMER:
-      text = '\ue863'
+    case RINGNETWORKCABINET /* 环网柜 */:
+      text = '\ue11d'
       break
-    case RINGNETWORKCABINET:
-      text = '\ue84f'
+    case ELECTRICITYDISTRIBUTIONROOM /* 配电室 */:
+      text = '\ue11a'
       break
-    case ELECTRICITYDISTRIBUTIONROOM:
-      text = '\ue85b'
+    case SWITCHINGSTATION /* 开闭所 */:
+      text = '\ue125'
       break
-    case SWITCHINGSTATION:
-      text = '\ue849'
+    case COLUMNCIRCUITBREAKER /* 柱上断路器 */:
+      text = '\ue11a'
       break
-    case COLUMNCIRCUITBREAKER:
-      text = '\ue85a'
+    case COLUMNTRANSFORMER /* 柱上变压器 */:
+      text = '\ue118'
       break
-    case COLUMNTRANSFORMER:
-      text = '\ue84a'
-      break
-    case CABLEBRANCHBOX:
-      text = '\ue845'
+    case CABLEBRANCHBOX /* 电缆分支箱 */:
+      text = '\ue123'
       break
 
     default:
@@ -56,7 +86,7 @@ export const pointStyle = (data: any, selected: boolean = false) => {
   }
   return new Style({
     text: new Text({
-      font: 'Normal 22px webgisIconFont',
+      font: 'Normal 52px ' + font,
       text,
       fill: new Fill({
         color: color,
