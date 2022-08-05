ddaf57c6fc98ea2e2150008591b34e18b98d2ac0
rxq <1909669885@qq.com>
2022-08-05 17:21:05 +0800

    feat: 网架管理优化,设备筛选高亮开发

--- src/pages/visualization-results/grid-manage/toolbar/index.tsx
+++ src/pages/visualization-results/grid-manage/toolbar/index.tsx
@@ -1,5 +1,5 @@
 import { getlinesComponment, getrepeatPointdata } from '@/services/grid-manage/treeMenu'
-import { AimOutlined, SearchOutlined, CloseOutlined } from '@ant-design/icons'
+import { AimOutlined, SearchOutlined, CloseOutlined, BulbOutlined } from '@ant-design/icons'
 import { useRequest } from 'ahooks'
 import { Button, Checkbox, Form, message, Space, Spin, Table, Input } from 'antd'
 import { useEffect, useState } from 'react'
@@ -36,6 +36,9 @@ interface RepeatPointType {
 }
 const { useForm } = Form
 const { Search } = Input
+const initialFeatureType = FEATUREOPTIONS.filter(
+  (item) => item.value !== POWERSUPPLY && item.value !== TRANSFORMERSUBSTATION
+)
 
 const Toolbar = (props: { leftMenuVisible: boolean }) => {
   const { leftMenuVisible } = props
@@ -61,10 +64,16 @@ const Toolbar = (props: { leftMenuVisible: boolean }) => {
   const [tableData, setTableData] = useState<RepeatPointType[]>([])
   // copy一份查重数据
   const [copyRepeateData, setCopyRepeateData] = useState<RepeatPointType[]>([])
+  // copy一份搜索数据
+  const [copySearchData, setCopySearchData] = useState<RepeatPointType[]>([])
   // 搜索列表是否显示
   const [searchState, setSearchState] = useState(false)
   // 搜索关键字
   const [keyWord, setKeyWord] = useState<string>('')
+  // 按设备筛,当前选中设备
+  const [selectedFeatureType, setSelectedFeatureType] = useState<string[]>(
+    initialFeatureType.map((item) => item.value)
+  )
   // 表格标题
   const columns = [
     {
@@ -143,10 +152,14 @@ const Toolbar = (props: { leftMenuVisible: boolean }) => {
   const searchEquipmentType = (value: any) => {
     const lineIds = currentChecklineIds()
     if (!lineIds.length) {
+      message.info('请勾选线路')
       return
     }
+    setSelectedFeatureType(form.getFieldValue('featureType'))
     getSearchEquipmentTypeData()
   }
+  /** 根据设备展示数据弹窗高亮按钮 **/
+  const highlightHandle = (value: any) => {}
 
   // 当前选中的主线线路
   const currentChecklineIds = () => {
@@ -208,27 +221,41 @@ const Toolbar = (props: { leftMenuVisible: boolean }) => {
       message.info('请勾选线路')
       return
     }
+    const linesAndPoints = getShowLines(mapRef.map).concat(getShowPoints(mapRef.map))
+    if (linesAndPoints && linesAndPoints.length === 0) {
+      // 地图未渲染
+      message.info('请等待地图绘制完成后搜索')
+      return
+    }
     setSearchState(!searchState)
     repeatPointState && setRepeatPointState(false)
     !searchState && setTableData([])
     !searchState && setKeyWord('')
+    const filterData = linesAndPoints
+      .map((item: any) => {
+        return item.get('data')
+      })
+      .sort((a: any, b: any) => {
+        // 没有名字的点排在数组尾部
+        if (!a.name) {
+          return 1
+        }
+        return -1
+      })
+    setTableData(filterData)
+    setCopySearchData(filterData)
   }
   // 搜索
   const searchEvent = () => {
     if (searchState) {
       // 当前弹窗为搜索
       if (keyWord.trim() === '') {
-        setTableData([])
+        setTableData(copySearchData)
         return
       }
-      const linesAndPoints = getShowLines(mapRef.map).concat(getShowPoints(mapRef.map))
-      const filterData = linesAndPoints
-        .map((item: any) => {
-          return item.get('data')
-        })
-        .filter((item: any) => {
-          return !!item.name && item.name.indexOf(keyWord) !== -1
-        })
+      const filterData = copySearchData.filter((item: any) => {
+        return !!item.name && item.name.indexOf(keyWord) !== -1
+      })
       setTableData(filterData)
     } else {
       // 当前弹窗为查重
@@ -302,26 +329,40 @@ const Toolbar = (props: { leftMenuVisible: boolean }) => {
           right: toolbalHasShow ? `${drawToolbarVisible || pageDrawState ? 398 : 20}px` : -200,
         }}
       >
-        <Form
-          style={{ zIndex: 999 }}
-          form={form}
-          onFinish={searchEquipmentType}
-          initialValues={ininFromData}
-        >
-          <Form.Item name="featureType">
-            <Checkbox.Group
-              className="EditFeature"
-              options={FEATUREOPTIONS.filter(
-                (item) => item.value !== POWERSUPPLY && item.value !== TRANSFORMERSUBSTATION
-              )}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              确定
-            </Button>
-          </Form.Item>
-        </Form>
+        <div style={{ width: '130px' }}>
+          <Form
+            style={{ zIndex: 999 }}
+            form={form}
+            onFinish={searchEquipmentType}
+            initialValues={ininFromData}
+          >
+            <Form.Item name="featureType">
+              <Checkbox.Group className="EditFeature" options={initialFeatureType} />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block style={{ width: '140px' }}>
+                确定
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        <div className={styles.highlightBtnWrap}>
+          {initialFeatureType.map((item) => {
+            if (selectedFeatureType.includes(item.value)) {
+              return (
+                <div
+                  onClick={() => {
+                    highlightHandle(item.value)
+                  }}
+                  className={styles.highlightBtn}
+                >
+                  <BulbOutlined />
+                </div>
+              )
+            }
+            return <div style={{ height: '22px' }}></div>
+          })}
+        </div>
       </div>
       <Button
         type="primary"
