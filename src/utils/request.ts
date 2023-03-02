8b43823b2fc28acc9121b940c8597a60a078ec76
huangzhuo <darkmoon4195@qq.com>
2023-03-02 14:25:21 +0800

    feat(转接口): request拦截器调整

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,10 +5,15 @@ const request = extend({})
 // request拦截器, 改变url 或 options.
 // @ts-ignore
 request.interceptors.request.use(async (url: string, options: RequestOptionsInit) => {
+  let host = window.location.host
   let c_token = localStorage.getItem('Authorization')
+  let accessUrl = options.method === 'get' ? '/commonGet' : '/commonPost' //穿透接口
+  let targetUrl = encodeURIComponent(`${host}${url}`) //目标接口转码
+
   const { headers } = options
   if (c_token) {
     return {
+      // url: `${accessUrl}?targetUrl=${targetUrl}`,
       url: url,
       options: {
         ...options,
@@ -24,6 +29,7 @@ request.interceptors.request.use(async (url: string, options: RequestOptionsInit
 
   return {
     url: url,
+    // url: `${accessUrl}?targetUrl=${targetUrl}`,
     options: {
       ...options,
       headers: {
