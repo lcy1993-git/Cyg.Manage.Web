9a87b505da1c0b7aaaf462aa04336362e5331cd1
huangzhuo <darkmoon4195@qq.com>
2023-03-10 20:11:52 +0800

    test(添加url转义): test

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -13,7 +13,7 @@ request.interceptors.request.use(async (url: string, options: RequestOptionsInit
   // let targetUrl = encodeURIComponent(`${protocol}//${host}${handleUrl}`) //目标接口转码
   let handleUrl = url.includes('bbgl') ? url.slice(23) : url
   let targetPort = getServiceIP(url)
-  let targetUrl = `http://${targetPort}${handleUrl}` //目标接口转码
+  let targetUrl = encodeURIComponent(`http://${targetPort}${handleUrl}`) //目标接口转码
   let isJson = url.includes('/json')
 
   const { headers } = options
