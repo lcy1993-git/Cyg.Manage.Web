276f05f1019a2d6979bc4fa64ed3cd8e08a45d01
huangzhuo <darkmoon4195@qq.com>
2023-03-30 14:28:05 +0800

    fix(提交项目全过程): 修改Post参数query方式

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,15 +6,14 @@ const request = extend({})
 // request拦截器, 改变url 或 options.
 // @ts-ignore
 request.interceptors.request.use(async (url: string, options: RequestOptionsInit) => {
-  let host = window.location.host
   let c_token = localStorage.getItem('Authorization')
   let accessUrl = options.method === 'get' ? '/commonGet' : '/commonPost' //穿透接口
-  let handleUrl = url.includes('bbgl') ? url.slice(23) : url
-  let targetUrl = encodeURIComponent(`https://srthkf1.gczhyun.com:21530${handleUrl}`) //目标接口转码
+  // let handleUrl = url.includes('bbgl') ? url.slice(23) : url
+  // let targetUrl = encodeURIComponent(`https://srthkf1.gczhyun.com:21530${handleUrl}`) //目标接口转码
 
   // let targetPort = getServiceIP(url)
 
-  // let targetUrl = encodeURIComponent(url.includes('bbgl') ? url : `http://172.2.48.22${url}`) //目标接口转码
+  let targetUrl = encodeURIComponent(url.includes('bbgl') ? url : `http://172.2.48.22${url}`) //目标接口转码
   let isJson = url.includes('/json')
   let isBbgl = url.includes('bbgl')
   let isNoGlzz = isJson || isBbgl
@@ -23,11 +22,11 @@ request.interceptors.request.use(async (url: string, options: RequestOptionsInit
   if (c_token) {
     return {
       // url: url,
-      // url: isNoGlzz ? url : `http://11.188.90.191:21525${accessUrl}?target_url=${targetUrl}`,
+      url: isNoGlzz ? url : `http://11.188.90.191:21525${accessUrl}?target_url=${targetUrl}`,
       // url: isNoGlzz ? url : `http://10.6.1.111:8082${accessUrl}?target_url=${targetUrl}`,
-      url: isNoGlzz
-        ? url
-        : `https://srthkf1.gczhyun.com:21530/glzz${accessUrl}?target_url=${targetUrl}`,
+      // url: isNoGlzz
+      //   ? url
+      //   : `https://srthkf1.gczhyun.com:21530/glzz${accessUrl}?target_url=${targetUrl}`,
       options: {
         ...options,
         headers: {
@@ -42,11 +41,11 @@ request.interceptors.request.use(async (url: string, options: RequestOptionsInit
 
   return {
     // url: url,
-    // url: isNoGlzz ? url : `http://11.188.90.191:21525${accessUrl}?target_url=${targetUrl}`,
+    url: isNoGlzz ? url : `http://11.188.90.191:21525${accessUrl}?target_url=${targetUrl}`,
     // url: isNoGlzz ? url : `http://10.6.1.111:8082${accessUrl}?target_url=${targetUrl}`,
-    url: isNoGlzz
-      ? url
-      : `https://srthkf1.gczhyun.com:21530/glzz${accessUrl}?target_url=${targetUrl}`,
+    // url: isNoGlzz
+    //   ? url
+    //   : `https://srthkf1.gczhyun.com:21530/glzz${accessUrl}?target_url=${targetUrl}`,
     options: {
       ...options,
       headers: {
